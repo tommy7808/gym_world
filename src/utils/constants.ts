d95a0b0ccd58c8d25412b336db7622c964fca288
Ayotomiwa Adekunle <ayotomiwaadekunle7808@gmail.com>
2024-11-16 14:59:52 +0000

    feat: setup tailwind config with fonnts & colours

---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+export enum Heading {
+    h1 = 'font-semibold text-mobile-h1 sm:text-tablet-h1 lg:text-desktop-h1',
+    h2 = 'font-semibold text-mobile-h2 sm:text-tablet-h2 lg:text-desktop-h2',
+    h3 = 'font-semibold text-mobile-h3 sm:text-tablet-h3 lg:text-desktop-h3',
+    h4 = 'font-semibold text-mobile-h4 sm:text-tablet-h4 lg:text-desktop-h4',
+}
+
+export enum Paragraph {
+    p1 = 'text-mobile-p1 sm:text-tablet-p1 lg:text-desktop-p1',
+    p2 = 'text-mobile-p2 sm:text-tablet-p2 lg:text-desktop-p2',
+    p3 = 'text-mobile-p3 sm:text-tablet-p3 lg:text-desktop-p3',
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,56 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          grey: {
+            '100': '#333131',
+            '200': '#242323',
+            '300': '#131212',
+          },
+          black: {
+            '100': '#E4E4E4',
+            '200': '#D4D4D4',
+            '300': '#ABABAB',
+          },
+        },
+        primary: {
+          '100': '#F3535E',
+          '200': '#F2404C',
+          '300': '#F02D3A',
+          '400': '#EF1A28',
+          '500': '#E5101E',
+          '600': '#D20F1C',
+          '700': '#BF0D19',
+          '800': '#AC0C17',
+          '900': '#990B14',
+        }
+      },
+      fontSize: {
+        'desktop-h1': '4rem',
+        'desktop-h2': '3rem',
+        'desktop-h3': '2.5rem',
+        'desktop-h4': '1.75rem',
+        'desktop-p1': '1.5rem',
+        'desktop-p2': '1.25rem',
+        'desktop-p3': '1rem',
+        'tablet-h1': '2.375rem',
+        'tablet-h2': '2.125rem',
+        'tablet-h3': '1.875rem',
+        'tablet-h4': '1.625rem',
+        'tablet-p1': '1.25rem',
+        'tablet-p2': '1rem',
+        'tablet-p3': '0.875rem',
+        'mobile-h1': '2rem',
+        'mobile-h2': '1.5rem',
+        'mobile-h3': '1.25rem',
+        'mobile-h4': '1.125rem',
+        'mobile-p1': '1rem',
+        'mobile-p2': '0.875rem',
+        'mobile-p3': '0.75rem',
+      },
+    }
   },
   plugins: [],
 }
